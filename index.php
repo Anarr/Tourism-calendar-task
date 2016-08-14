2b439bb17f20036760765e37a62ade2111d0d4ed
Anar <anar.rzayev94@gmail.com>
2016-08-14 20:28:27 +0400

    Add files via upload
    last changes

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8' />
+<link href='fullcalendar.css' rel='stylesheet' />
+<link href='fullcalendar.print.css' rel='stylesheet' media='print' />
+<script src='lib/moment.min.js'></script>
+<script src='lib/jquery.min.js'></script>
+<script src='fullcalendar.min.js'></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css" type="text/css">
+<script src="my-app.js" type="text/javascript"></script>
+<link href='https://fonts.googleapis.com/css?family=Orbitron:400,700' rel='stylesheet' type='text/css'>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img alt="Brand" src="static/img/logo.png" width="70">
+      </a>
+    </div>
+  </div>
+</nav>
+	
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-9 my-calendar-box">
+  <h2>Tourism Calendar</h2>
+	<div id='calendar'></div>
+	</div>
+
+
+<div class="col-md-3 my-form">
+<form class="form-horizontal">
+  <div class="form-group">
+    <label for="start-date" class="col-sm-2 control-label">Start</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="start-date" placeholder="start">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="end-date" class="col-sm-2 control-label">End</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="end-date" placeholder="end">
+    </div>
+  </div>
+<!--   <div class="form-group">
+    <label for="price" class="col-sm-2 control-label">Price</label>
+    	<div class="col-sm-10">
+        <input type="text" class="form-control" placeholder="price" id="price">
+        </div>
+  </div> -->
+</form>
+<div class="data-div col-sm-12 col-md-12" class="hide"></div>
+	</div>
+
+</div>
+</div>
+</body>
+</html>
---
+++ my-app.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+$( ".data-div" ).draggable({});
+// lsit seklinde olan melumat box-un gizledirik
+		$('.data-div').hide();
+
+		$('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next',
+				center: 'title',
+				right: ''
+			},
+			default:false,
+			// default olaraq bugunun tarixin secirik
+			defaultDate: new Date(),
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				
+				//var title = prompt('Event Title:');
+				var eventData;
+				var arr = [];
+				
+				//if (title) {
+					/*eventData = {
+						title: 'Anar',
+						start: start,
+						end: end
+					};*/
+
+					// baslangic ve son tarixi muvafiq formata gore gotrur ve "T" simvoluna gore split edib lazim 
+					//olan hisseni gotururuk
+
+					var start_d = moment(start).format().split("T")[0];
+					var end_d = moment(end).format().split("T")[0];
+					//baslangic ve son tarixleri muvafiq inputlara yaziriq
+
+					$('#start-date').val(start_d);
+					$('#end-date').val(end_d);
+					var price = $('#price').val();
+					//begin ajax request data.php file
+					$.ajax
+    				({ 
+        			url: 'data.php',
+        			data:{'start_d':start_d,'end_d':end_d,'price':price},
+        			dataType:'json',
+        			type: 'post',
+        			success: function(result)
+        			{
+        				//alert(JSON.stringify(result));die();
+        				// calendara custom qiymetler elave etmek ucun events massivi yaradiriq
+        				var events = [];
+        				var li = [];
+        				//$('.data-div').html(result[0]['start']);
+        				arr = result;
+        			
+        					for(var i =0; i < arr.length; i++) 
+							{events.push( {title: arr[i]['price'] , start:arr[i]['start'],end:end})}
+							//alert(JSON.stringify(events));die();
+
+							/*for(var j=0;j<arr.length;j++){
+									eventData = {
+									title: arr[j]['price'],
+									start: arr[j]['start'],
+									end: end
+									};
+					}*/
+
+					//muvafiq inputlari disable edirik
+
+					$("#start-date").prop('disabled', true);
+					$("#end-date").prop('disabled', true);
+					
+					//ilkin olaraq fullcalendar.io -un event massivin silib custom event massivimizi muvafiq xanalara yerlesdirik
+					$('#calendar').fullCalendar('removeEventSource', events);
+					$('#calendar').fullCalendar('addEventSource', events);
+
+					//list seklinde olan melumat box-una gelen data-ni jquery vasitesi ile yerlesdirirk
+					$.each(events,function(i,item){
+						li.push('<li class="list-group-item my-li-hover">Tarix: ' + item.start +" Qiymet: "+ item.title  + '<span class="glyphicon glyphicon-usd"></span></li>');
+					});
+
+					$('.data-div').append("<h2>Prices for relevant dates</h2><ul class='list-group my-list'><li class='list-group-item active'>Tarix: Qiymet:</li>"+ li.join(' ') +"</ul>").fadeIn(500);
+
+					localStorage.setItem("eve",  JSON.stringify(events));
+
+       				 },
+       				 async: false
+   				 });
+	
+
+
+    			//end ajax
+
+				$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
+				//}
+				$('#calendar').fullCalendar('unselect');
+				alert(localStorage.getItem("eve"));
+
+			},
+
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			// muvafiq qiymetlere click eden zaman custom css elave etirik
+			eventClick:function(){
+				$(this).css({'border-radius':'0','background':'green'});
+			}
+			/*events:[{
+				title:12,
+				start:"2016-08-08",
+				end:"2016-08-19"
+			}
+			],*/
+
+		});
+		
+	});
+
